068d428f899ae0bca980b539c1011bfa055c25b4
albertogioia93 <albertogioia@hotmail.it>
2023-03-27 22:54:31 +0200

    import javascript di bootstrap in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './assets/scss/main.scss';
+
+// per importare il javascript di bootstrap
+import * as bootstrap from 'bootstrap'
 import "@fontsource/montserrat"; // Defaults to weight 400.
 import App from './App.vue'
 
